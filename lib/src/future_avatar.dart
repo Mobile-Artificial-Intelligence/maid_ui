9da77009cd5b6ae5ca8b004e1fe6630a765c7059
danemadsen <dane_madsen@hotmail.com>
2024-04-26 19:08:51 +1000

    fix image updating

--- lib/src/future_avatar.dart
+++ lib/src/future_avatar.dart
@@ -6,26 +6,42 @@ class FutureAvatar extends StatelessWidget {
   final Future<File> image;
   final double? radius;
 
-  const FutureAvatar({super.key, required this.image, this.radius});
+  // Static cache map
+  static final Map<Key, File> _cache = {};
+
+  const FutureAvatar({required super.key, required this.image, this.radius});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<File>(
-      future: image,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return const Icon(Icons.error);
+    // Check if the image is already cached
+    final cachedImage = _cache[key!];
+
+    if (cachedImage != null) {
+      return CircleAvatar(
+        backgroundImage: FileImage(cachedImage),
+        radius: radius,
+      );
+    }
+    else {
+      return FutureBuilder<File>(
+        future: image,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return const Icon(Icons.error);
+            } else {
+              _cache[key!] = snapshot.data!;
+
+              return CircleAvatar(
+                backgroundImage: FileImage(snapshot.data!),
+                radius: radius,
+              );
+            }
           } else {
-            return CircleAvatar(
-              backgroundImage: FileImage(snapshot.data!),
-              radius: radius,
-            );
+            return const CircularProgressIndicator();
           }
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
-    );
+        },
+      );
+    }
   }
 }
